b9d4302ac20a8b80a92316a5a2e8c7d11898c71c
tim9han <alpauler@mail.ru>
2025-06-16 18:52:45 +0300

    libwebsocket.so build
    Script for building libwebsocket library without SSL for android platform (NDK r19+)

--- README.md
+++ README.md
@@ -1,67 +1,31 @@
-# buildLibWebSocket
-build libwebsocket
+The repository is based on https://github.com/honjane/buildLibWebSocket
 
-1.下载Libwebseocket库
+This repository is designed to build the libwebsocket.so file for use as http server/client in your project. 
 
-git clone https://github.com/warmcat/libwebsockets.git
+There have been significant changes in the NDK (NDK r19+), which required changes in honjane's script
+Major changes in the modern NDK:
 
-2.环境准备（Mac版）
+1. Clang instead of GCC:
+Old: arm-linux-androideabi-gcc
+New: armv7a-linux-androideabi21-clang
 
-2.1安装zlib ：
+2. Prebuilt toolchain:
+Old: Create a standalone toolchain
+New: Directly use $NDK/toolchains/llvm/prebuilt/
 
-brew install zlib
+3. API level in the compiler name:
+New approach includes API level in the name: ${TARGET}${API}-clang
 
-2.2安装makedepend：
+4. LLVM tools:
+llvm-ar, llvm-ranlib, llvm-strip instead of GNU versions
 
-brew install makedepend
+-------------------------------------
 
-2.3安装cmake ：
+To build the project, run the android-make-script-all.sh script in the directory buildws
 
-brew install cmake
+Use the build parameter:
+ARM|ARM64|X86|X86_64|ALL
 
-3.编译.a静态文件
+The build has been tested on android 11 and 12 as an http server.
 
-有了上面这些工具，准备工作差不多了，然后通过libwebsocket/contrib目录下的android-make-script.sh编译.a文件
-这个sh文件有些问题，比如：只能编译出arm架构的文件，并且在编译zlib库时使用的libtool有问题，libwebsocket原文件编译出错。
-
-针对这些问题作了一些修改：
-
-3.1 修改output.c和http2.c原文件编译错误，这个已经提交到libwebsocket base2.2版本上了
-  https://github.com/warmcat/libwebsockets/commit/34842d7492b728349f6a6898e3893b08d70625fa
-  
-3.2 替换libtool文件
-
-    mv ./Makefile ./Makefile.old
-    sed "s/AR=libtool/AR=`echo ${AR}|sed 's#\/#\\\/#g'`/" ./Makefile.old > Makefile.mid
-    sed "s/ARFLAGS=-o/ARFLAGS=-r/" ./Makefile.mid > Makefile
-    
-3.3 增加build x86架构文件编译
-
-4.封装websocket.
-
-4.1初始化 initLws
-
-4.2连接  connect
-
-4.3发送／接收消息 writeLws／callback
-
-4.4断开 exitLws
-
-5.搭建简易服务端测试
-
-python testServer.py 
-这个py服务器比较简单，只能支持1024字节传输，超过这个长度会乱码，要测试超长字符，服务器还是要自己去搭建。
-
-
-##使用步骤：
-
-1.进入buildws目录，运行sh文件 生成.a文件，然后把生成的.a文件拷贝到websocket/src/main/jni/ 对应架构下
-libcrypto.a libssl.a libwebsocket.a libz.a
-
-2.使用build-ndk 编译 jni目录生成对应libwebsocket.so 
-怎么编译就不多说，生成的so文件会在libs和obj目录下，把这些生存的so拷贝到jniLibs／对应架构下目录
-
-3.启动python服务器 ，连接服务器，发送消息
-
-
-
+!Attention! The repository uses only the http server, to run the https server you will need to build the libwebsocket library together with SSL (see honjane's repository)
--- buildws/android-make-script-all.sh
+++ buildws/android-make-script-all.sh
@@ -1,221 +1,209 @@
 #!/bin/bash
 
-#
-# Build libwebsockets static library for Android
-#
-# requires debian package xutils-dev for makedepend (openssl make depend)
-#
+# Build libwebsockets static library without SSL support for Android using modern NDK
 
-# This is based on http://stackoverflow.com/questions/11929773/compiling-the-latest-openssl-for-android/
-# via https://github.com/warmcat/libwebsockets/pull/502
+# Get the absolute path to the current directory where the script is located
+CURRENT_DIR=$(cd "$(dirname "$0")" && pwd) 
+SCRIPT_DIR=$CURRENT_DIR
 
-# your NDK path
-export NDK=/opt/Android/sdk/ndk-bundle
+set -e # end script at first mistake
 
-set -e
-
-# Download packages libz, openssl and libwebsockets
-
-[ ! -f zlib-1.2.8.tar.gz ] && {
-wget http://prdownloads.sourceforge.net/libpng/zlib-1.2.8.tar.gz
+##############################
+# Check the directory exists #
+##############################
+function check_directory(){
+if [ -d "$2" ]; then
+    echo "$1 path:"$'\n'" $2"
+else
+    echo "Not exist $1 path:"$'\n'" $2"
+    exit 1
+fi
 }
 
-[ ! -f openssl-1.0.2g.tar.gz ] && {
-wget https://openssl.org/source/openssl-1.0.2g.tar.gz
+#########################
+# Check the file exists #
+#########################
+function check_file(){
+    if [ -f "$2" ]; then
+        echo "$1 file:"$'\n'" $2"
+    else
+        echo "Not exist $1 file:"$'\n'" $2"
+        exit 1
+    fi
 }
 
-[ ! -f libwebsockets.tar.gz ] && {
-git clone https://github.com/warmcat/libwebsockets.git
-tar cvf libwebsockets.tar.gz libwebsockets
+
+# Set paths
+export NDK="$ANDROID_NDK"  # Your android NDK path
+export SDK="$ANDROID_SDK"  # Your android SDK path
+export CMAKE="$SDK/cmake/3.22.1/bin/cmake"  # Your CMAKE path in android SDK
+export API_LEVEL=21
+
+
+# Check paths
+check_directory "NDK" "$NDK"  # Check android NDK set in your system path
+check_directory "SDK" "$SDK"  # Check android SDK set in your system path
+check_file "CMAKE" "$CMAKE"   # Check CMAKE from android SDK
+
+
+# Download packages libwebsockets
+if [ ! -f libwebsockets.tar.gz ]; then
+  git clone https://github.com/warmcat/libwebsockets.git
+  check_directory "libwebsockets" "$SCRIPT_DIR/libwebsockets"  
+  cd libwebsockets 
+  # checkout supported git commit
+  git checkout 5102a5c8d6110b25a01492fcf96fb668b13dd6e7 
+  cd ..
+  # pack libwebsocket for next script 
+  tar czf libwebsockets.tar.gz libwebsockets
+fi
+
+# Create build directories
+
+# libwebsockets 
+cd "$SCRIPT_DIR/libwebsockets"
+rm -rf ./build
+mkdir build
+
+
+#########################################################
+# set compiler and archivator in dependense of platform #
+#########################################################
+function setup_toolchain() {
+    ABI=$1
+
+    UNAME=$(uname -s) # check current OS
+    ARCH=$(uname -m)  # check CPU architecture
+
+
+    # set HOST_TAG value in depence of current OS and CPU architecture
+    case "$UNAME" in
+        Linux)
+            HOST_TAG=linux-x86_64
+            ;;
+        Darwin)
+            case "$ARCH" in
+                arm64)
+                    HOST_TAG=darwin-arm64
+                    ;;
+                *)
+                    HOST_TAG=darwin-x86_64
+                    ;;
+            esac
+            ;;
+        MINGW*|MSYS*|CYGWIN*)
+            HOST_TAG=windows-x86_64
+            ;;
+        *)
+            echo "Unsupported system: $UNAME"
+            exit 1
+            ;;
+    esac
+
+    # set toolchain path
+    TOOLCHAIN="$NDK/toolchains/llvm/prebuilt/$HOST_TAG"
+    export AR="$TOOLCHAIN/bin/llvm-ar"
+
+    case "$ABI" in
+        armeabi-v7a)
+            export CC="$TOOLCHAIN/bin/armv7a-linux-androideabi$API_LEVEL-clang"
+            ;;
+        arm64-v8a)
+            export CC="$TOOLCHAIN/bin/aarch64-linux-android$API_LEVEL-clang"
+            ;;
+        x86)
+            export CC="$TOOLCHAIN/bin/i686-linux-android$API_LEVEL-clang"
+            ;;
+        x86_64)
+            export CC="$TOOLCHAIN/bin/x86_64-linux-android$API_LEVEL-clang"
+            ;;
+        *)
+            echo "Unknown ABI: $ABI"
+            exit 1
+            ;;
+    esac
 }
 
-# create a local android toolchain
+##############################
+# Build libwebsockets module #
+##############################
+function build_libwebsockets() {
+    # setup toolchain compiler and archivator
+    ABI=$1
+    BUILD_DIR=$2
+    setup_toolchain "$ABI"
+    echo "Building libwebsockets for $ABI..."
 
-export FILE_PATH=
-export TOOLCHAIN_PATH=
-export TOOL=
-export ARCH_FLAGS=
-export HEADERS=
-export LIBS=
-export OPENSSL_ARCH=
-export OPENSSL_PARAMS=
+    mkdir -p "$BUILD_DIR"
+    cd "$BUILD_DIR"
 
+    $CMAKE -DCMAKE_POLICY_DEFAULT_CMP0057=NEW "$SCRIPT_DIR/libwebsockets" \
+        -DCMAKE_TOOLCHAIN_FILE=$NDK/build/cmake/android.toolchain.cmake \
+        -DANDROID_ABI=$ABI \
+        -DANDROID_PLATFORM=android-$API_LEVEL \
+        -DCMAKE_BUILD_TYPE=Release \
+        -DCMAKE_C_COMPILER="$CC" \
+        -DCMAKE_AR="$AR" \
+        -DCMAKE_INSTALL_PREFIX=$2 \
+        -DLWS_WITH_STATIC=ON \
+        -DLWS_WITH_SHARED=OFF \
+        -DLWS_WITHOUT_TESTAPPS=ON \
+        -DLWS_WITH_SSL=OFF \
+        -DLWS_WITH_HTTP2=ON \
+        -DLWS_USE_BUNDLED_ZLIB=OFF \
+        -DLWS_WITH_ZLIB=OFF \
+        -DLWS_WITHOUT_DAEMONIZE=ON \
+        -DLWS_IPV6=OFF \
+        -DLWS_WITH_PLUGINS=OFF \
+        -DLWS_ROLE_DBUS=OFF \
+        -DLWS_CMAKE_WARNINGS_ARE_ERRORS=OFF \
+        -DLWS_WITHOUT_BUILTIN_GETIFADDRS=OFF \
+        -DLWS_WITH_GETIFADDRS=OFF \
+        -DLWS_WITH_GETGRNAM=OFF \
+        -DLWS_WITH_GETGRGID=OFF \
+        -DLWS_NO_LOGS=ON \
+        -DCMAKE_SHARED_LINKER_FLAGS="-Wl,--gc-sections" \
+        -DCMAKE_C_FLAGS="-fvisibility=hidden -ffunction-sections -fdata-sections -Wno-error=unused-label -Wno-error=sign-conversion -std=c11 -DLWS_WITH_GETIFADDRS=0 -DLWS_WITH_GETGRNAM=0 -DLWS_WITH_GETGRGID=0"
 
-function build_ws(){
+    make -j$(nproc)
+    make install
 
-    # Clean then Unzip
-    [ -d zlib-1.2.8 ] && rm -fr zlib-1.2.8
-    [ -d openssl-1.0.2g ] && rm -fr openssl-1.0.2g
-    [ -d libwebsockets ] && rm -fr libwebsockets
-    tar xf zlib-1.2.8.tar.gz
-    tar xf openssl-1.0.2g.tar.gz
-    tar xzf libwebsockets.tar.gz
-
-    echo " clean success "
-
-    # setup environment to use the gcc/ld from the android toolchain
-    export NDK_TOOLCHAIN_BASENAME=${TOOLCHAIN_PATH}/${TOOL}
-    export CC=$NDK_TOOLCHAIN_BASENAME-gcc
-	export CXX=$NDK_TOOLCHAIN_BASENAME-g++
-	export LINK=${CXX}
-	export LD=$NDK_TOOLCHAIN_BASENAME-ld
-	export AR=$NDK_TOOLCHAIN_BASENAME-ar
-	export RANLIB=$NDK_TOOLCHAIN_BASENAME-ranlib
-	export STRIP=$NDK_TOOLCHAIN_BASENAME-strip
-
-	# setup buildflags
-	export ARCH_LINK=
-	export CPPFLAGS=" ${ARCH_FLAGS} -I${HEADERS} -fpic -ffunction-sections -funwind-tables -fstack-protector -fno-strict-aliasing -finline-limit=64 "
-	export CXXFLAGS=" ${ARCH_FLAGS} -I${HEADERS} -fpic -ffunction-sections -funwind-tables -fstack-protector -fno-strict-aliasing -finline-limit=64 -frtti -fexceptions "
-	export CFLAGS=" ${ARCH_FLAGS} -I${HEADERS} -fpic -ffunction-sections -funwind-tables -fstack-protector -fno-strict-aliasing -finline-limit=64 "
-	export LDFLAGS=" ${ARCH_LINK} "
-
-	# configure and build zlib
-	[ ! -f ./${FILE_PATH}/lib/libz.a ] && {
-	cd zlib-1.2.8
-	PATH=$TOOLCHAIN_PATH:$PATH ./configure --static --prefix=$TOOLCHAIN_PATH/..
-    mv ./Makefile ./Makefile.old
-    sed "s/AR=libtool/AR=`echo ${AR}|sed 's#\/#\\\/#g'`/" ./Makefile.old > Makefile.mid
-    sed "s/ARFLAGS=-o/ARFLAGS=-r/" ./Makefile.mid > Makefile
-	PATH=$TOOLCHAIN_PATH:$PATH make
-	PATH=$TOOLCHAIN_PATH:$PATH make install
-	cd ..
-	}
-
-	# configure and build openssl
-	[ ! -f ./${FILE_PATH}/lib/libssl.a ] && {
-	PREFIX=$TOOLCHAIN_PATH/..
-	cd openssl-1.0.2g
-
-	./Configure ${OPENSSL_ARCH} --prefix=${PREFIX} ${OPENSSL_PARAMS}
-	PATH=$TOOLCHAIN_PATH:$PATH make depend
-	PATH=$TOOLCHAIN_PATH:$PATH make
-	PATH=$TOOLCHAIN_PATH:$PATH make install_sw
-	cd ..
-	}
-
-	# configure and build libwebsockets
-	[ ! -f ./${FILE_PATH}/lib/libwebsockets.a ] && {
-	cd libwebsockets
-	[ ! -d build ] && mkdir build
-	cd build
-	PATH=$TOOLCHAIN_PATH:$PATH cmake \
-	  -DCMAKE_C_COMPILER=$CC \
-	  -DCMAKE_AR=$AR \
-	  -DCMAKE_RANLIB=$RANLIB \
-	  -DCMAKE_C_FLAGS="$CFLAGS" \
-	  -DCMAKE_INSTALL_PREFIX=$TOOLCHAIN_PATH/.. \
-	  -DLWS_WITH_SHARED=OFF \
-	  -DLWS_WITH_STATIC=ON \
-	  -DLWS_WITHOUT_DAEMONIZE=ON \
-	  -DLWS_WITHOUT_TESTAPPS=ON \
-	  -DLWS_IPV6=OFF \
-	  -DLWS_USE_BUNDLED_ZLIB=OFF \
-	  -DLWS_WITH_SSL=ON  \
-	  -DLWS_WITH_HTTP2=ON \
-	  -DLWS_OPENSSL_LIBRARIES="$TOOLCHAIN_PATH/../lib/libssl.a;$TOOLCHAIN_PATH/../lib/libcrypto.a" \
-	  -DLWS_OPENSSL_INCLUDE_DIRS=$TOOLCHAIN_PATH/../include \
-	  -DCMAKE_BUILD_TYPE=Release \
-	  ..
-	PATH=$TOOLCHAIN_PATH:$PATH make
-	PATH=$TOOLCHAIN_PATH:$PATH make install
-	cd ../..
-	}
-
-	echo " build success"
+    # Сlear unnecessary files after build complete
+    find "$BUILD_DIR" -mindepth 1 -maxdepth 1 ! -name lib ! -name include -exec rm -rf {} +
+    echo "Build for $ABI completed: $BUILD_DIR"
+    cd ../..
 }
 
-#build arm
-function build_for_arm(){
-    # rm dir
-    [ -d android-toolchain-arm ] && rm -fr android-toolchain-arm
 
-	export FILE_PATH=android-toolchain-arm
-	export TOOLCHAIN_PATH=`pwd`/${FILE_PATH}/bin
-	export TOOL=arm-linux-androideabi
-	export ARCH_FLAGS="-mthumb"
-	export HEADERS=`pwd`/${TOOLCHAIN_PATH}/sysroot/usr/include
-	export LIBS=`pwd`/${TOOLCHAIN_PATH}/sysroot/usr/lib
-	export OPENSSL_ARCH="android"
-	export OPENSSL_PARAMS="no-shared no-idea no-mdc2 no-rc5 no-zlib no-zlib-dynamic enable-tlsext no-ssl2 no-ssl3 enable-ec enable-ecdh enable-ecp"
-	
-	$NDK/build/tools/make-standalone-toolchain.sh \
-	--platform=android-9 \
-	--toolchain=arm-linux-androideabi-4.9 \
-	--install-dir=`pwd`/${FILE_PATH}
-	build_ws
+function build_all() {
+    ABI=$1
+    OUTPUT_LIBWEBSOCKET_DIR="$SCRIPT_DIR/build/build-android-$ABI"
+  
+    # Build libwebsockets module
+    build_libwebsockets $ABI $OUTPUT_LIBWEBSOCKET_DIR 
+    echo "Build for $ABI completed. Output in $OUTPUT_LIBWEBSOCKET_DIR"
 }
 
-#build x86
+case "$1" in
+    ARM | arm)
+        build_all armeabi-v7a
+        ;;
+    ARM64 | arm64)
+        build_all arm64-v8a
+        ;;
+    X86 | x86)
+        build_all x86
+        ;;
+    X86_64 | x86_64)
+        build_all x86_64
+        ;;
+    ALL | all)
+        for abi in armeabi-v7a arm64-v8a x86 x86_64; do
+            build_all $abi
+        done
+        ;;
+    *)
+        echo "Usage: $0 {ARM|ARM64|X86|X86_64|ALL}"
+        ;;
+esac
 
-function build_for_x86(){
-    #rm dir
-    [ -d android-toolchain-x86 ] && rm -fr android-toolchain-x86
-
-	export FILE_PATH=android-toolchain-x86
-	export TOOLCHAIN_PATH=`pwd`/${FILE_PATH}/bin
-	export TOOL=i686-linux-android
-	export ARCH_FLAGS="-march=i686 -msse3 -mstackrealign -mfpmath=sse"
-	export HEADERS=`pwd`/${TOOLCHAIN_PATH}/sysroot/usr/include
-	export LIBS=`pwd`/${TOOLCHAIN_PATH}/sysroot/usr/lib
-	export OPENSSL_ARCH="android-x86"
-	export OPENSSL_PARAMS="no-asm no-shared no-idea no-mdc2 no-rc5 no-zlib no-zlib-dynamic enable-tlsext no-ssl2 no-ssl3 enable-ec enable-ecdh enable-ecp"
-
-	$NDK/build/tools/make-standalone-toolchain.sh \
-	--platform=android-9 \
-	--toolchain=x86-4.9 \
-	--install-dir=`pwd`/${FILE_PATH}
-	build_ws
-}
-
-#build x86_64
-function build_for_x86_64(){
-    #rm dir
-    [ -d android-toolchain-x86-64 ] && rm -fr android-toolchain-x86-64
-
-	export FILE_PATH=android-toolchain-x86_64
-	export TOOLCHAIN_PATH=`pwd`/${FILE_PATH}/bin
-	export TOOL=x86_64-linux-android
-	export ARCH_FLAGS=
-	export HEADERS=`pwd`/${TOOLCHAIN_PATH}/sysroot/usr/include
-	export LIBS=`pwd`/${TOOLCHAIN_PATH}/sysroot/usr/lib
-	export OPENSSL_ARCH="linux-x86_64"
-	export OPENSSL_PARAMS="no-asm no-shared no-idea no-mdc2 no-rc5 no-zlib no-zlib-dynamic enable-tlsext no-ssl2 no-ssl3 enable-ec enable-ecdh enable-ecp enable-ec_nistp_64_gcc_128"
-
-	$NDK/build/tools/make-standalone-toolchain.sh \
-	--platform=android-9 \
-	--toolchain=x86_64-4.9 \
-	--install-dir=`pwd`/${FILE_PATH}
-	build_ws
-}
-#build based on input
-function judge_params(){
-	if [ $1 -eq 0 ];
-	then echo "Please enter your build target."
-	return
-	fi
-	if [ $1 -gt 1 ];
-	then echo "Too many parameters."
-	return
-	fi
-	export input=$(echo $2 | tr '[a-z]' '[A-Z]')
-	case ${input} in
-	"ARM") echo "***build for arm***"
-		build_for_arm
-		;;
-	"X86") echo "***build for X86***"
-		build_for_x86
-		;;
-	"X86_64") echo "***build for X86***"
-		build_for_x86_64
-		;;
-	"ALL") echo "***build for all architecture***"
-		build_for_arm
-		build_for_x86
-		build_for_x86_64
-		;;
-	*) echo "Please enter a valid parameter."
-		;;
-	esac
-}
-judge_params $# $1
